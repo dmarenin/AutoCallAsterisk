422799439d9e0c6cb1b79c0eafdc88503238f807
le9ra6 <mexanik_96@mail.ru>
2019-01-21 18:48:16 +0500

    upd ignore

--- common/conf.py
+++ common/conf.py
@@ -1,14 +1,14 @@
-AMI_SECRET = '' 
-AMI_USER = ''
+AMI_SECRET = 'GJKBvth22' 
+AMI_USER = 'bear'
 AMI_PORT = 5038 
-AMI_ADDRESS = '' 
+AMI_ADDRESS = '192.168.5.73' 
 
 
 APP = 'PlayBack'
-DATA = '/usr/local/share/asterisk/moh//auto/sps'
+DATA = '/usr/local/share/asterisk/moh/agrad/auto/sps'
 
 
-FILE_NAMES = {'kia':'file_name=/usr/local/share/asterisk/moh/agrad//auto_kia', 
-              'ford':'file_name=/usr/local/share/asterisk/moh/agrad//auto_ford', 
-              'renault':'file_name=/usr/local/share/asterisk/moh/agrad//auto_renault'}
+FILE_NAMES = {'kia':'file_name=/usr/local/share/asterisk/moh/agrad/auto/auto_kia', 
+              'ford':'file_name=/usr/local/share/asterisk/moh/agrad/auto/auto_ford', 
+              'renault':'file_name=/usr/local/share/asterisk/moh/agrad/auto/auto_renault'}
 
